d714419246841fe71161b552a55cf6de293de9c1
dev.ryu2021 <dev.ryu2021@gmail.com>
2023-09-11 16:30:49 +0900

    init: 프로젝트 초기화

---
+++ src/main/java/com/hypeboy/hypeBoard/HypeBoardApplication.java
@@ -0,0 +1,13 @@
+package com.hypeboy.hypeBoard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HypeBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HypeBoardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hypeboy/hypeBoard/HypeBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hypeboy.hypeBoard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HypeBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
