71769ee3e798517543c310fe22e76a48a9ee62da
dev.ryu2021 <dev.ryu2021@gmail.com>
2023-09-14 14:41:35 +0900

    샘플 컨트롤러 및 페이지 추가 - 초기화면 못 찾는 이슈가 있어서 properties에 경로 지정 설정

---
+++ src/main/java/com/hypeboy/hypeBoard/UserController.java
@@ -0,0 +1,13 @@
+package com.hypeboy.hypeBoard;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class UserController {
+
+    @GetMapping("/")
+    public String home() {
+        return "home";
+    }
+}
