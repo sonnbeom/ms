e402215ab9b85037d2682a720b3752e68fbf80a1
dev.ryu2021 <dev.ryu2021@gmail.com>
2023-09-15 13:43:12 +0900

    유저 dto에 id 항목 추가

--- src/main/java/com/ms/controller/UserController.java
+++ src/main/java/com/ms/controller/UserController.java
@@ -1,8 +1,10 @@
 package com.ms.controller;
 
 import com.ms.dto.UserDto;
+import com.ms.service.UserServiceImpl;
 import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,13 +14,18 @@ import org.springframework.web.bind.annotation.RequestBody;
 @Controller("/users")
 public class UserController {
 
+    @Autowired
+    private UserServiceImpl userService;
+
     @PostMapping("/signup")
     public String signup(@RequestBody @Valid UserDto dto, Errors errors) {
         if (errors.hasErrors()) {
-            log.info("userDto invalid: >>" + errors.getFieldError());
+            log.info("/signup invalid: >>" + errors.getFieldError());
             return "home";
         }
 
+        log.info("/signup controller: >> " + dto);
+        userService.save(dto);
 
         return "home";
     }
--- src/main/java/com/ms/dto/UserDto.java
+++ src/main/java/com/ms/dto/UserDto.java
@@ -6,6 +6,9 @@ import lombok.Data;
 
 @Data
 public class UserDto {
+    @NotNull(message = "아이디를 입력해주세요.")
+    private String id;
+    
     @NotNull(message = "이름을 입력해주세요.")
     private String name;
 
