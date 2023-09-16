e96636bcc0c74c023414e42059b2a1d120778eda
devsince2021 <dev.ryu2021@gmail.com>
2023-09-17 04:26:59 +0900

    Chore: 코드 변경으로 발생한 미사용 코드 제거 (#20)

--- src/main/java/com/hypeboy/hypeBoard/service/UserService.java
+++ src/main/java/com/hypeboy/hypeBoard/service/UserService.java
@@ -43,15 +43,6 @@ public class UserService {
 
         throw new NoSuchElementException("사용자를 찾을 수 없습니다.");
     }
-  
-    public List<User> pwdJoin(String pwd){
-        List<User> pwdCheck =  userRepository.findByPwd(pwd);
-        if (pwdCheck.size()==0){
-            throw new NoSuchElementException("비밀번호가 존재하지 않습니다.");
-        }
-        return pwdCheck;
-    }
-  
     public User idJoin(String id){
         return userRepository.findById(id).
                 orElseThrow(() -> new NoSuchElementException("아이디가 존재하지 않습니다."));
