c8bc49b5f8a53835f6d2661d14a30d732f2f0df1
devsince2021 <dev.ryu2021@gmail.com>
2023-09-18 00:04:57 +0900

    Feat: 로그인/로그아웃 로직 Spring Security로 이동 (#25)

--- src/main/java/com/hypeboy/hypeBoard/controller/UserController.java
+++ src/main/java/com/hypeboy/hypeBoard/controller/UserController.java
@@ -2,22 +2,15 @@ package com.hypeboy.hypeBoard.controller;
 
 import com.hypeboy.hypeBoard.dto.UserDto;
 import com.hypeboy.hypeBoard.service.UserService;
-import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/user")
-@SessionAttributes("user")
 public class UserController {
     @Autowired
     private UserService userService;
-
-    @Autowired
-    private BCryptPasswordEncoder passwordEncoder;
 
     @GetMapping("signup")
     public String signup(){
@@ -34,16 +27,9 @@ public class UserController {
     public String callLoginPage(){
         return "login";
     }
-  
-    @PostMapping("/myPage")
-    public String login(@RequestParam("id") String id, @RequestParam("pwd") String pwd, Model model){
-        model.addAttribute("user",userService.join(id,pwd));
-        return "mypage";
-    }
 
-    @PostMapping("/logout")
-    public String logout(HttpSession session){
-        session.invalidate();
-        return "login";
+    @GetMapping("/content")
+    public String contentSample() {
+        return "content";
     }
 }
---
+++ src/main/java/com/hypeboy/hypeBoard/entity/UserDetailsCustom.java
@@ -0,0 +1,52 @@
+package com.hypeboy.hypeBoard.entity;
+
+import com.hypeboy.hypeBoard.enums.UserRole;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+public class UserDetailsCustom implements UserDetails {
+    private User user;
+
+    public UserDetailsCustom(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Arrays.asList(new SimpleGrantedAuthority(UserRole.ROLE_USER.getValue()));
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPwd();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getId();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
